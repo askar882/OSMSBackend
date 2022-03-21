5907fd270cd1ee25c3bc14f1de8666260ea2ac11
askar882 <17901680+askar882@users.noreply.github.com>
2022-03-21 10:01:30 +0800

    搭建Spring Boot项目框架。

---
+++ src/main/java/com/oas/osmsbackend/OsmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.oas.osmsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsmsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oas/osmsbackend/OsmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oas.osmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsmsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
