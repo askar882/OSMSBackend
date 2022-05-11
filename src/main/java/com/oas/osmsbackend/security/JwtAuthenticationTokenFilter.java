e66a1e022ff0a1359110ff80cfb03020ad2a1822
askar882 <17901680+askar882@users.noreply.github.com>
2022-05-11 09:52:58 +0800

    修改鉴权查询逻辑，添加注释。

--- src/main/java/com/oas/osmsbackend/security/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/oas/osmsbackend/security/JwtAuthenticationTokenFilter.java
@@ -1,30 +1,28 @@
 package com.oas.osmsbackend.security;
 
+import com.oas.osmsbackend.config.AppConfiguration;
 import com.oas.osmsbackend.util.Constants;
 import com.oas.osmsbackend.util.ResponseUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.ConfigAttribute;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.FilterInvocation;
-import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.util.Collection;
-import java.util.List;
 
 /**
+ * JWT身份认证过滤器。
+ * 继承{@link OncePerRequestFilter}类来表示对一个请求只执行一次，防止{@link Component}被扫描自动添加的最低优先级实例被重新调用。
  * @author askar882
  * @date 3/31/2022
  */
@@ -32,12 +30,25 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+    /**
+     * JWT验证提供器。
+     */
     private final JwtTokenProvider jwtTokenProvider;
+    /**
+     * 应用配置，用于获取忽略的URL列表。
+     */
+    private final AppConfiguration appConfiguration;
 
+    /**
+     * 过滤逻辑。
+     * @param request 过滤的{@link HttpServletRequest}对象。
+     * @param response 过滤的{@link HttpServletResponse}对象。
+     * @param chain 过滤链。
+     */
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         log.debug("Filtering request URI: '{}'", request.getRequestURI());
-        if (!requiresAuthentication(request, response, chain)) {
+        if (!requiresAuthentication(request)) {
             log.debug("Authentication ignored.");
             chain.doFilter(request, response);
             return;
@@ -61,20 +72,19 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
         chain.doFilter(request, response);
     }
 
-    private boolean requiresAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain) {
-        try {
-            Field additionalFiltersField = chain.getClass().getDeclaredField("additionalFilters");
-            additionalFiltersField.setAccessible(true);
-            @SuppressWarnings("unchecked")
-            List<Filter> additionalFilters = (List<Filter>) additionalFiltersField.get(chain);
-            FilterSecurityInterceptor interceptor = (FilterSecurityInterceptor) additionalFilters.get(additionalFilters.size() - 1);
-            Collection<ConfigAttribute> attributes = interceptor.obtainSecurityMetadataSource()
-                    .getAttributes(new FilterInvocation(request, response, chain));
-            log.debug("attributes: {}", attributes.toString());
-            return attributes.stream().noneMatch(attribute -> "permitAll".equals(attribute.toString()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return true;
+    /**
+     * 检查请求是否需要鉴权。
+     * @param request 检查的请求。
+     * @return 如需鉴权，返回{@code true}，否则返回{@code false}。
+     */
+    private boolean requiresAuthentication(HttpServletRequest request) {
+        return appConfiguration.getIgnoredUrls().stream().map(matcher -> {
+            String pattern = matcher.getPattern();
+            HttpMethod method = matcher.getMethod();
+            if (method != null) {
+                return new AntPathRequestMatcher(pattern, method.name());
+            }
+            return new AntPathRequestMatcher(pattern);
+        }).anyMatch(matcher -> matcher.matches(request));
     }
 }
